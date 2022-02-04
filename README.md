5d68a704cd6cf4b209b277aa381ad66aa2a1f5d2
Stefano <34969+abaddon@users.noreply.github.com>
2022-02-04 13:28:28 +0000

    fix (+4 squashed commits) Squashed commits: [b61f465] code cleaning [7589abc] no message [0490e83] no message [d17b343] first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kustomCompare
-A library to compare classes 
+[![Java CI with Gradle](https://github.com/abaddon/kustomCompare/actions/workflows/gradle.yml/badge.svg)](https://github.com/abaddon/kustomCompare/actions/workflows/gradle.yml)
+[![codecov](https://codecov.io/gh/abaddon/kustomCompare/branch/main/graph/badge.svg?token=N24T6BXQB8)](https://codecov.io/gh/abaddon/kustomCompare)
+
+A library to compare classes
---
+++ build.gradle.kts
@@ -0,0 +1,158 @@
+group = "io.github.abaddon"
+version = "0.0.1"
+
+object Meta {
+    const val desc = "A library to compare classes"
+    const val license = "Apache-2.0"
+    const val githubRepo = "abaddon/kustomCompare"
+    const val release = "https://s01.oss.sonatype.org/service/local/"
+    const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
+
+object Versions {
+    const val slf4jVersion = "1.7.25"
+    const val kotlinVersion = "1.6.0"
+    const val kotlinCoroutineVersion = "1.6.0"
+    const val jacksonModuleKotlinVersion = "2.13.0"
+    const val junitJupiterVersion = "5.7.0"
+    const val jacocoToolVersion = "0.8.7"
+    const val jvmTarget = "11"
+}
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+    jacoco
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    signing
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("kustomCompare") {
+            from(components["kotlin"])
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:${Versions.slf4jVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlinVersion}")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:${Versions.junitJupiterVersion}") // JVM dependency
+}
+
+jacoco {
+    toolVersion = Versions.jacocoToolVersion
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
+    }
+}
+
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+signing {
+    val signingKey = providers
+        .environmentVariable("GPG_SIGNING_KEY")
+        .forUseAtConfigurationTime()
+    val signingPassphrase = providers
+        .environmentVariable("GPG_SIGNING_PASSPHRASE")
+        .forUseAtConfigurationTime()
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions
+            .getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(components["kotlin"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+            pom {
+                name.set(project.name)
+                description.set(Meta.desc)
+                url.set("https://github.com/${Meta.githubRepo}")
+                licenses {
+                    license {
+                        name.set(Meta.license)
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hpehl")
+                        name.set("Harald Pehl")
+                        organization.set("Red Hat")
+                        organizationUrl.set("https://developers.redhat.com/")
+                    }
+                }
+                scm {
+                    url.set(
+                        "https://github.com/${Meta.githubRepo}.git"
+                    )
+                    connection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                    developerConnection.set(
+                        "scm:git:git://github.com/${Meta.githubRepo}.git"
+                    )
+                }
+                issueManagement {
+                    url.set("https://github.com/${Meta.githubRepo}/issues")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri(Meta.release))
+            snapshotRepositoryUrl.set(uri(Meta.snapshot))
+            val ossrhUsername = providers
+                .environmentVariable("OSSRH_USERNAME")
+                .forUseAtConfigurationTime()
+            val ossrhPassword = providers
+                .environmentVariable("OSSRH_PASSWORD")
+                .forUseAtConfigurationTime()
+            if (ossrhUsername.isPresent && ossrhPassword.isPresent) {
+                username.set(ossrhUsername.get())
+                password.set(ossrhPassword.get())
+            }
+        }
+    }
+}
